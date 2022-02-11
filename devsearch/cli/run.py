d4a3a13456bfd25c4624b7c2d8d46947a5865e2a
MassterMax <59767630+MassterMax@users.noreply.github.com>
2022-02-11 14:02:45 +0300

    working with dulwich

--- devsearch/cli/run.py
+++ devsearch/cli/run.py
@@ -1,5 +1,7 @@
 import click
 
+from devsearch.preprocessing.extract import extract_repo
+
 
 @click.group()
 def cli():
@@ -10,6 +12,13 @@ def cli():
 @click.argument("message")
 def hello(message: str):
     print(f"you wrote: {message}")
+
+
+@cli.command()
+@click.argument("git_path")
+@click.argument("target_path", type=click.Path())
+def extract(git_path: str, target_path: str):
+    extract_repo(git_path, target_path)
 
 
 if __name__ == '__main__':
