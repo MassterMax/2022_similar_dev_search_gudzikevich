106cfbc8e9a2d40502eaef4259f1fda05f027034
MassterMax <59767630+MassterMax@users.noreply.github.com>
2022-02-16 13:29:18 +0300

    add docstrings

--- devsearch/cli/run.py
+++ devsearch/cli/run.py
@@ -12,6 +12,15 @@ def cli():
 @click.argument("git_path")
 @click.argument("target_path", type=click.Path())
 def extract(git_path: str, target_path: str):
+    """
+    A command to extract info about commits from one repository
+    Args:
+        git_path: Repository GitHub URL
+        target_path: Where to store (if not stored yet) the repository on your device
+
+    Returns:
+
+    """
     extract_repo(git_path, target_path)
 
 
