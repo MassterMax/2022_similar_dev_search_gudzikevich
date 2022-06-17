732c7e5d6ac290f2a6c0475346cd5261df98f245
MassterMax <maxmail877@gmail.com>
2022-06-17 23:35:46 +0300

    add multiple languages to identifiers extract

--- devsearch/tree_sitter/parsing.py
+++ devsearch/tree_sitter/parsing.py
@@ -6,6 +6,23 @@ from devsearch.tree_sitter.setup import parser_library
 
 logger = logging.getLogger(__name__)
 
+LANGUAGE_TO_QUERY = {
+    "python": """(
+        (identifier) @all_identifiers
+    )""",
+    "go": """
+           (short_var_declaration left: (expression_list (identifier)) @var_name)
+           (type_spec name: (type_identifier) @class_name)
+           (function_declaration name: (identifier) @func_name)
+           """,
+    "javascript": """(
+        (identifier) @all_identifiers
+    )""",
+    "default": """(
+    (identifier) @all_identifiers
+    )"""
+}
+
 
 @lru_cache(maxsize=None)
 def get_identifiers_with_query(language: str, source_code: bytes) -> Iterator[str]:
@@ -19,10 +36,7 @@ def get_identifiers_with_query(language: str, source_code: bytes) -> Iterator[st
 
     try:
         parser = parser_library.get_parser(language)
-        query = parser_library.get_language(language).query(
-            """(
-              (identifier) @constant
-            )""")  # todo maybe specify identifier to any language
+        query = parser_library.get_language(language).query(LANGUAGE_TO_QUERY.get(language, "default"))
 
         captures = query.captures(parser.parse(source_code).root_node)
         for capture in captures:
